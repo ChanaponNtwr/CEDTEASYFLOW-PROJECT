09afa78b6e4f786cefb8645b7deb5da89e66393d
NTPH09 <147842474+NTPH09@users.noreply.github.com>
2025-09-16 23:39:00 +0700

    .

---
+++ easyflow/types/next-auth.d.ts
@@ -0,0 +1,25 @@
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    } & DefaultSession["user"];
+  }
+
+  interface User extends DefaultUser {
+    id: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    name?: string | null;
+    email?: string | null;
+    picture?: string | null;
+  }
+}
